906638e6e23ad60a8824c76fd2b675ff95037189
av1401 <rsabhi1401@gmail.com>
2025-03-05 19:54:59 +0530

    [Abhishek] add . validating name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   imgUrl = "assets\\Logo.jpg";
   url = "https://www.bridgelabz.com"
   userName: string ="Abhishek";
+  nameError: string= "";
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";
@@ -18,5 +19,17 @@ export class AppComponent {
   onClick($event: any){
     console.log("Save button is clicked!", $event);
     window.open(this.url,"_blank");
+  }
+
+  onInput($event: Event) {
+    const inputElement = $event.target as HTMLInputElement;
+    this.userName = inputElement.value; // Update userName from input field
+  
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+    } else {
+      this.nameError = "Name is incorrect";
+    }
   }
 }
