6150d897c1457f1ccf687320f8271755fe6fa350
av1401 <rsabhi1401@gmail.com>
2025-03-05 19:32:27 +0530

    [Abhishek] add . clicking image to open url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-<<<<<<< HEAD
   title = 'HelloWorld';
+  imgUrl = "assets\\Logo.jpg";
+  url = "https://www.bridgelabz.com"
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";
   }
 
-=======
-  title = 'Hello From BridgeLabz';
-  imgUrl = "assets\\Logo.jpg";
->>>>>>> UC2
+  onClick($event: any){
+    console.log("Save button is clicked!", $event);
+    window.open(this.url,"_blank");
+  }
 }
